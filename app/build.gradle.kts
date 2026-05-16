f483ceccb5e3faae2d132c978afcf4230616ec16
Nelson da Silva <nelsondsd@gmail.com>
2026-05-16 20:21:13 +0200

    chore: bump version to 1.2.2 (8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.neldasi.dafscanner"
         minSdk = 27
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.2.1"
+        versionCode = 8
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
